a5301c8e33a75b4241d656a7a4fc6613f10b9292
amanjai35 <144518699+amanjai35@users.noreply.github.com>
2023-09-09 17:10:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  res.end('Hello, World! your website is deployed successfully\n');
 });
 
 const port = process.env.PORT || 3000;
